8e90e36cab640cb707897fa80796d0385b3f14a0
Minati Alphonso <Minati.Alphonso@mines.sdsmt.edu>
2022-02-21 14:19:10 -0700

    Added EndActivity, GameplayActivity, and CaptureActivity

---
+++ app/src/main/java/edu/sdsmt/project1/GameplayActivity.java
@@ -0,0 +1,14 @@
+package edu.sdsmt.project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gameplay);
+    }
+}
