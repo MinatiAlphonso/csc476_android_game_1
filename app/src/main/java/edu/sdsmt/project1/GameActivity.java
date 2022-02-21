9c31e31ee2ffefc8770dc38bc9e89a81abf5b24f
Minati Alphonso <Minati.Alphonso@mines.sdsmt.edu>
2022-02-21 14:26:36 -0700

    Refactor GameplayActivity to GameActivity.

--- app/src/main/java/edu/sdsmt/project1/GameActivity.java
+++ app/src/main/java/edu/sdsmt/project1/GameActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class GameplayActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gameplay);
+        setContentView(R.layout.activity_game);
     }
 }
