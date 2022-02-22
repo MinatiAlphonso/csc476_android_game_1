193666a790dd40027d67f3546eef6970007cf997
h44k13 <h44k13@gmail.com>
2022-02-22 15:12:21 -0700

    Added some code to Game and Player class skeletons.

--- app/src/main/java/edu/sdsmt/project1/Player.java
+++ app/src/main/java/edu/sdsmt/project1/Player.java
@@ -6,36 +6,41 @@ import java.io.Serializable;
 
 public class Player {
 
-    public Player() {
+    private Parameters params;
 
+    public Player() {
+        params = new Parameters();
     }
 
     public String getName() {
-        return "";
+
+        return params.name;
     }
 
     public void setName(String name) {
-
+        params.name = name;
     }
 
     public int getScore() {
-        return 0;
+        return params.score;
     }
 
     // Increment the players score when the collect a item
     public void scored() {
-
+        params.score += 1;
     }
 
     public static class Parameters implements Serializable {
+        private int score = 0;
+        private String name = null;
 
     }
     // Serialize and store parameters in bundle
-    public void savePlayer(Bundle bundle) {
-
+    public void savePlayer(String key, Bundle bundle) {
+        bundle.putSerializable(key, params);
     }
     // Load serializable
-    public void restorePlayer(Bundle bundle) {
-
+    public void restorePlayer(String key, Bundle bundle) {
+        params = (Parameters)bundle.getSerializable(key);
     }
 }
