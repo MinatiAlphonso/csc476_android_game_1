ba7236ac63a1d76add721e301c1beab087528381
Minati Alphonso <Minati.Alphonso@mines.sdsmt.edu>
2022-02-22 20:25:01 -0700

    Added a Capture button to game layout to finalize the capture action.

--- app/src/main/java/edu/sdsmt/project1/GameActivity.java
+++ app/src/main/java/edu/sdsmt/project1/GameActivity.java
@@ -2,7 +2,6 @@ package edu.sdsmt.project1;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -10,8 +9,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -31,12 +28,12 @@ public class GameActivity extends AppCompatActivity {
 
         //Get the message from the intent (StartActivity started up this activity)
         Intent intent = getIntent();
-        TextView player1name = findViewById(R.id.player1name);
-        player1name.setText(String.format("%s%s", getString(R.string.player1_text),intent.getStringExtra(PLAYER1_NAME)));
-        TextView player2name = findViewById(R.id.player2name);
-        player2name.setText(String.format("%s%s", getString(R.string.player2_text),intent.getStringExtra(PLAYER2_NAME)));
-        TextView roundCount = findViewById(R.id.roundCount);
-        roundCount.setText(String.format("%s%s", getString(R.string.round_text), intent.getIntExtra(ROUND_COUNT,0)));
+        TextView Player1Name = findViewById(R.id.textViewPlayer1Name);
+        Player1Name.setText(String.format("%s%s", getString(R.string.player1_text),intent.getStringExtra(PLAYER1_NAME)));
+        TextView Player2Name = findViewById(R.id.textViewPlayer2Name);
+        Player2Name.setText(String.format("%s%s", getString(R.string.player2_text),intent.getStringExtra(PLAYER2_NAME)));
+        TextView RoundCount = findViewById(R.id.textViewRoundCount);
+        RoundCount.setText(String.format("%s%s", getString(R.string.round_text), intent.getIntExtra(ROUND_COUNT,0)));
 
         ActivityResultContracts.StartActivityForResult contract =
                 new ActivityResultContracts.StartActivityForResult();
