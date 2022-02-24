697cb53c55fd40e7aff40f996de93ae302b9b03c
7429138 <liam.bell@mines.sdsmt.edu>
2022-02-24 16:35:33 -0700

    added code for closing and returning data from captureActivity

--- app/src/main/java/edu/sdsmt/project1/CaptureActivity.java
+++ app/src/main/java/edu/sdsmt/project1/CaptureActivity.java
@@ -2,13 +2,41 @@ package edu.sdsmt.project1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CaptureActivity extends AppCompatActivity {
+
+    private String captureType = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capture);
     }
+
+    public void onRectClick(View view) {
+        captureType = "RECTANGLE";
+        close();
+    }
+
+    public void onCircleClick(View view) {
+        captureType = "CIRCLE";
+        close();
+    }
+
+    public void onLineClick(View view) {
+        captureType = "LINE";
+        close();
+    }
+
+    private void close() {
+        Intent intent = new Intent();
+        intent.putExtra("CAPTURE_TYPE", captureType);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+
 }
